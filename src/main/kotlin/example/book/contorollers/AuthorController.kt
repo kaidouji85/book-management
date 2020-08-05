4a7ad67868ee1a06ea9c7f7728725aed4cf94a21
竹内佑介 <kaidouji85@gmail.com>
2020-08-05 12:37:36 +0900

    DBへのインサートを追加した

--- src/main/kotlin/example/book/contorollers/AuthorController.kt
+++ src/main/kotlin/example/book/contorollers/AuthorController.kt
@@ -4,6 +4,7 @@ import example.book.entity.Author
 import example.book.repository.AuthorRepository
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import javax.inject.Inject
 
 /**
@@ -15,8 +16,15 @@ class AuthorController {
     lateinit var authorRepository: AuthorRepository
 
     @Get("/")
-    fun index(): List<Author> {
+    fun all(): List<Author> {
         return authorRepository.findAll()
                 .toList();
+    }
+
+    @Post("/")
+    fun insert(): String {
+        val author = Author(0, "author")    //TODO リクエストパラメータからパースする
+        authorRepository.save(author)
+        return "success";
     }
 }
