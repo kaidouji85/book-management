335c7eb73b2879eba81a6c85ea7b6222c39eedff
竹内佑介 <kaidouji85@gmail.com>
2020-08-06 19:00:49 +0900

    著者一覧のスタイル調整をした

--- browser-client/src/app/authors/index.tsx
+++ browser-client/src/app/authors/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AuthorInfo, fetchAllAuthors} from "../api/authors";
 import {Author} from "./author";
+import '../../css/authors/index.css';
 
 /**
  * 著者管理コンポネント プロパティ
@@ -52,7 +53,7 @@ export function AuthorsPresentation(props: AuthorsState) {
   return (
     <div className="authors">
       <div className="authors__title">著者情報</div>
-      <div className="authors__table">
+      <div className="authors__items">
         {props.authors.map(v => Author({author: v}))}
       </div>
     </div>
