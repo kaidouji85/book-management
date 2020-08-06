acf0c126fc37d1caacc6d147b3eb55d77343a6c8
竹内佑介 <kaidouji85@gmail.com>
2020-08-06 17:46:28 +0900

    ブラウザ側のsrc配下構造を変更した

--- browser-client/src/index.tsx
+++ browser-client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/index.css';
+import App from './app/App';
+import * as serviceWorker from './app/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
